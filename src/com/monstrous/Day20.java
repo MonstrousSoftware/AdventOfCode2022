3880785e8b0975c0962103f43d085aaf4eb355cf
Marco <marco.monster@gmail.com>
2022-12-22 00:25:09 +0100

    day 20

--- src/com/monstrous/Day20.java
+++ src/com/monstrous/Day20.java
@@ -4,12 +4,14 @@ import java.util.HashMap;
 
 public class Day20 {
 
+    final static long DECRYPTION_KEY = 811589153;
+
     class Node {
-        int value;
+        long value;
         Node next;
         Node prev;
 
-        public Node(int value) {
+        public Node(long value) {
             this.value = value;
             next = null;
             prev = null;
@@ -24,7 +26,7 @@ public class Day20 {
             tail = null;
         }
 
-        public Node addNode(int value){
+        public Node addNode(long value){
             Node node = new Node(value);
             if(head == null) {
                 head = node;
@@ -49,22 +51,20 @@ public class Day20 {
 
         input = new FileInput("data/day20.txt");
 
-        int num = input.size();
-        int max = num-1;
-        System.out.println("size of array: "+num);
-        int[] initial = new int[ num ];
+        int[] initial = new int[ input.size() ];
 
         int index = 0;
         for(String line: input.lines) {
-
             int value = Integer.parseInt(line);
             initial[index++] = value;
         }
 
 
-        int sum = scrambleNew(initial);
-
+        long sum = scramble(initial, 1, 1);
         System.out.println("Part 1: sum is "+sum);
+
+        long sum2 = scramble(initial, DECRYPTION_KEY, 10);
+        System.out.println("Part 2: sum is "+sum2);
 
         final long endTime = System.currentTimeMillis();
         System.out.println("\nTotal execution time : " + (endTime - startTime) + " ms");
@@ -84,59 +84,64 @@ public class Day20 {
     }
 
 
-    private int scrambleNew( int start[] ) {
+    private long scramble( int start[], long key , int rounds) {
         int num = start.length;
+        long max = num-1;
         CircularList circle = new CircularList();
         Node[] initialList = new Node[num];
         Node zeroNode  = null;
 
         for(int i = 0 ; i < num; i++) {
-            Node node = circle.addNode(start[i]);
+            Node node = circle.addNode((long)start[i] * key);
             initialList[i] = node;
             if(node.value == 0)
                 zeroNode = node;
         }
 
-        //printCircle(circle);
+        if(num < 20)
+            printCircle(circle);
+        for(int round = 0; round < rounds; round++) {
+            for (int i = 0; i < num; i++) {
+                Node node = initialList[i];         // next value from initial sequence
 
-        for(int i = 0; i < num; i++) {
-            Node node = initialList[i];         // next value from initial sequence
+                long value = node.value;
 
-            int value = node.value;
+                // unlink from the list
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                if (circle.head == node)
+                    circle.head = node.next;
+                if (circle.tail == node)
+                    circle.tail = node.prev;
 
-            // unlink from the list
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            if(circle.head == node)
-                circle.head = node.next;
-            if(circle.tail == node)
-                circle.tail = node.prev;
+                //printCircle(circle);
 
-            //printCircle(circle);
-
-            Node curr = node;
-            if(value > 0) {
-                for (int j = 0; j < value; j++)
-                    curr = curr.next;
-            } else if(value < 0) {
-                for (int j = 0; j > value; j--)
+                Node curr = node;
+                value = ((value % max) + max)%max;
+                if (value > 0) {
+                    for (int j = 0; j < value; j++)
+                        curr = curr.next;
+                } else if (value < 0) {
+                    for (int j = 0; j > value; j--)
+                        curr = curr.prev;
                     curr = curr.prev;
-                curr = curr.prev;
-            } else if(value == 0)
-                curr = node.prev;
+                } else if (value == 0)
+                    curr = node.prev;
 
-            // insert node after curr
-            //System.out.println("Insert "+value+" between "+curr.value +" and "+curr.next.value);
-            node.next = curr.next;
-            curr.next = node;
-            node.prev = curr;
-            node.next.prev = node;
+                // insert node after curr
+                //System.out.println("Insert "+value+" between "+curr.value +" and "+curr.next.value);
+                node.next = curr.next;
+                curr.next = node;
+                node.prev = curr;
+                node.next.prev = node;
 
-            if(num < 20)
+
+            }
+            if (num < 20)
                 printCircle(circle);
         }
 
-        int sum = 0;
+        long sum = 0;
         Node curr = zeroNode;
         for(int sample = 1; sample <= 3; sample ++)
         {
