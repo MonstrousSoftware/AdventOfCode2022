533a6a6486fe322a71fd74ff96a6caa49bebb6cf
Monstrous <marco.monster@gmail.com>
2022-12-23 23:01:21 +0100

    day 22

--- src/com/monstrous/Day22.java
+++ src/com/monstrous/Day22.java
@@ -1,7 +1,6 @@
 package com.monstrous;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Day22 {
 
@@ -15,7 +14,7 @@ public class Day22 {
         public final int dx;
         public final int dy;
         public final char rep;
-        private Dir(int code, int dx, int dy, char rep) {
+        Dir(int code, int dx, int dy, char rep) {
             this.code = code;
             this.dx = dx;
             this.dy = dy;
@@ -25,7 +24,7 @@ public class Day22 {
     }
 
 
-    class Player {
+    static class Player {
         int x;
         int y;
         Dir facing;
@@ -38,13 +37,14 @@ public class Day22 {
 
     }
 
-    class Edge {
+    static class Edge {
         int startx;
         int starty;
         Dir edgeDir;
         int length;
         Dir facing;
         Edge adjoins;
+        char label;
 
         public Edge(int startx, int starty, Dir edgeDir, int len, Dir facing) {
             this.startx = startx;
@@ -52,17 +52,18 @@ public class Day22 {
             this.edgeDir = edgeDir;
             this.length = len;
             this.facing = facing;
+            this.label = '-';
         }
     }
 
-    int EDGE_LEN = 4;
-
-    final FileInput input = new FileInput("data/day22a.txt");
+    final int EDGE_LEN;
+    final FileInput input = new FileInput("data/day22.txt");
     final ArrayList<Edge> edges = new ArrayList<>();
     int cols;
-    int rows;
-    char [][] grid;
-    char [][] edgeDemo;
+    final int rows;
+    final char [][] grid;
+    final char [][] edgeDemo;
+    int part;
 
 
     public Day22() {
@@ -79,10 +80,6 @@ public class Day22 {
         rows = input.lines.size() - 2;	// -2 for separator line and path
         EDGE_LEN = Math.abs(rows - cols);
 
-
-        System.out.println("rows = "+rows);
-        System.out.println("cols = "+cols);
-        System.out.println("edge = "+EDGE_LEN);
         String path = null;
         grid = new char[rows][cols];
         edgeDemo = new char[rows][cols];    // for visualisation
@@ -111,63 +108,65 @@ public class Day22 {
         }
 
         findPivots(grid);
-        fillEdges();
 
 
-
-        // find start
-        Player player = new Player();
-        player.x = 0;
-        player.y = 0;
-        player.facing = Dir.RIGHT;
 
         //printGrid(grid, player);
+        //showEdges();
+        for(part = 1; part <= 2; part++) {
 
-        while(grid[player.y][player.x] != '.')
-            player.x++;
+            // find start
+            Player player = new Player();
+            player.x = 0;
+            player.y = 0;
+            player.facing = Dir.RIGHT;
 
-        printGrid(grid, player);
-        showEdges();
-
-        for(int index = 0; index < path.length(); index++) {
-            char k = path.charAt(index);
-            if(k == 'R' || k == 'L') {
-                player.facing = turn(k, player.facing);
-                //System.out.println("Turn "+k);
-            }
-            else if(isDigit(k) ) {
-                // accumulate digits to make an integer
-                int dist = k - '0';
-                while( index < path.length()-1 && isDigit(path.charAt(index+1)) ) {
-                    index++;
-                    dist = 10*dist + path.charAt(index) - '0';
-                }
-                //System.out.println("Forward "+dist);
-                // advance step by step
-                for(int step = 0; step < dist; step++){
-                    int nx = player.x + player.facing.dx;
-                    int ny = player.y + player.facing.dy;
-                    // are we on the map edge?
-                    if(nx >= cols || nx < 0 ||ny >= rows || ny < 0 || grid[ny][nx] == ' ' )  {	// hit the void, teleport
-                        //System.out.println("Teleport");
-                        if(!teleport2(grid, player))
-                            break;
-                    }
-                    else if ( grid[ny][nx] == '.' )
-                            player.advance();    // go into space
-                    else if ( grid[ny][nx] == '#' ) {    // blocked, stop advance loop
-                            //System.out.println("Blocked");
-                            break;
-                    }
-                    //printGrid(grid, player);
-                }
-            }
             //printGrid(grid, player);
+
+            while(grid[player.y][player.x] != '.')
+                player.x++;
+
+            for (int index = 0; index < path.length(); index++) {
+                char k = path.charAt(index);
+                if (k == 'R' || k == 'L') {
+                    player.facing = turn(k, player.facing);
+                } else if (isDigit(k)) {
+                    // accumulate digits to make an integer
+                    int dist = k - '0';
+                    while (index < path.length() - 1 && isDigit(path.charAt(index + 1))) {
+                        index++;
+                        dist = 10 * dist + path.charAt(index) - '0';
+                    }
+                    // advance step by step
+                    for (int step = 0; step < dist; step++) {
+                        int nx = player.x + player.facing.dx;
+                        int ny = player.y + player.facing.dy;
+                        // are we on the map edge?
+                        if (nx >= cols || nx < 0 || ny >= rows || ny < 0 || grid[ny][nx] == ' ') {    // hit the void, teleport
+                            boolean teleported;
+                            if(part == 1)
+                                teleported = teleportPart1(grid, player);
+                            else
+                                teleported = teleport(grid, player);
+                            if (!teleported)
+                                break;
+                        } else if (grid[ny][nx] != '#') {
+                            player.advance();    // go into empty space
+                        } else if (grid[ny][nx] == '#') {    // blocked, stop advance loop
+                            break;
+                        }
+                    }
+                }
+            }
+
+            //printGrid(grid, player);
+            //System.out.println("position at end of path: ("+(1+player.x)+" , "+(1+player.y)+") facing: "+player.facing.code);
+            int positionCode = 1000 * (1 + player.y) + 4 * (1 + player.x) + player.facing.code;    // check
+            System.out.println("Part "+part+": position code at end of path: " + positionCode);
         }
-        //printGrid(grid, player);
-        System.out.println("position at end of path: ("+(1+player.x)+" , "+(1+player.y)+") facing: "+player.facing.code);
-        int positionCode = 1000* (1+player.y) + 4 * (1+player.x) + player.facing.code;	// check
-        System.out.println("position code at end of path: "+positionCode);
+
+        //  part 1: 31568
+        //  part 2: 36540
 
         final long endTime = System.currentTimeMillis();
         System.out.println("\nTotal execution time : " + (endTime - startTime) + " ms");
@@ -176,7 +175,7 @@ public class Day22 {
     // player is standing facing the void, go in opposite direction until you meet the
     // void at the other side of the island
 
-    private boolean teleport(char [][]grid, Player player) {
+    private boolean teleportPart1(char [][]grid, Player player) {
         int rows = grid.length;
         int cols = grid[0].length;
         int nx = player.x - player.facing.dx;	// move in reverse
@@ -218,13 +217,14 @@ public class Day22 {
     }
 
     private void showEdges() {
-        char label = 'A';
+
         for(Edge edge : edges ){
             for(int i = 0; i < EDGE_LEN; i++) {
                 int x = edge.startx + i*edge.edgeDir.dx;
                 int y = edge.starty + i*edge.edgeDir.dy;
-                edgeDemo[y][x] = label;
+                edgeDemo[y][x] = edge.label;
             }
+            System.out.println("Edge "+edge.label+" ("+edge.startx+","+edge.starty+") dir:"+edge.edgeDir.rep+" normal:"+edge.facing);
             label++;
         }
         for(int y = 0; y < rows; y++) {
@@ -236,19 +236,23 @@ public class Day22 {
         System.out.println();
     }
 
+    private char label = 'A';
 
     private void linkEdges(Edge edge1, Edge edge2) {
         edge1.adjoins = edge2;
         edge2.adjoins = edge1;
         edges.add(edge1);
         edges.add(edge2);
-        System.out.println("join edges "+edge1.startx+","+edge1.starty+" "+edge1.edgeDir.rep+ " with "+
-                                        edge2.startx+","+edge2.starty+" "+edge2.edgeDir.rep);
+        edge1.label = label++;
+        edge2.label = label++;
+
+//        System.out.println("join edges "+edge1.startx+","+edge1.starty+" "+edge1.edgeDir.rep+ " with "+
+//                                        edge2.startx+","+edge2.starty+" "+edge2.edgeDir.rep);
     }
     private void pivotEdges(int pivx, int pivy, Dir eDir1, Dir eDir2, Dir dir1, Dir dir2 ) {
-        System.out.println("Pivot "+pivx+" , "+pivy);
-        Edge edge1 = new Edge(pivx+eDir1.dx,pivy+eDir1.dy, eDir1, EDGE_LEN, dir1);
-        Edge edge2 = new Edge(pivx+eDir2.dx,pivy+eDir2.dy, eDir2, EDGE_LEN, dir2);
+        //System.out.println("Pivot "+pivx+" , "+pivy);
+        Edge edge1 = new Edge(pivx + eDir1.dx, pivy + eDir1.dy, eDir1, EDGE_LEN, dir1);
+        Edge edge2 = new Edge(pivx + eDir2.dx, pivy + eDir2.dy, eDir2, EDGE_LEN, dir2);
         linkEdges(edge1, edge2);
 
         boolean void1, void2;
@@ -257,28 +261,29 @@ public class Day22 {
             int y1 = edge1.starty + EDGE_LEN * edge1.edgeDir.dy;
             int x2 = edge2.startx + EDGE_LEN * edge2.edgeDir.dx;
             int y2 = edge2.starty + EDGE_LEN * edge2.edgeDir.dy;
-            System.out.println("follow up " + x1 + "," + y1 + "  vs " + x2 + "," + y2);
+            //System.out.println("follow up " + x1 + "," + y1 + "  vs " + x2 + "," + y2);
             void1 = false;
             if (x1 < 0 || x1 >= cols || y1 < 0 || y1 >= rows || grid[y1][x1] == ' ') {
-                System.out.println("x1, y1 in void");
+                //System.out.println("x1, y1 in void");
                 void1 = true;
             }
             void2 = false;
             if (x2 < 0 || x2 >= cols || y2 < 0 || y2 >= rows || grid[y2][x2] == ' ') {
-                System.out.println("x2, y2 in void");
+                //System.out.println("x2, y2 in void");
                 void2 = true;
             }
             if (void1 && !void2) {
                 x1 = edge1.startx + (EDGE_LEN-1) * edge1.edgeDir.dx;        // end of edge1
                 y1 = edge1.starty + (EDGE_LEN-1) * edge1.edgeDir.dy;
-                edge1 = new Edge(x1, y1, opposite(edge2.edgeDir), EDGE_LEN, opposite(edge1.edgeDir)); // turn 90 degrees
+
+                edge1 = new Edge(x1, y1, edge1.facing, EDGE_LEN, opposite(edge1.edgeDir)); // turn 90 degrees into the block
                 edge2 = new Edge(x2, y2, edge2.edgeDir, EDGE_LEN, edge2.facing); // extend edge
                 linkEdges(edge1, edge2);
             }
             else if (void2 && !void1) {
                 x2 = edge2.startx + (EDGE_LEN-1) * edge2.edgeDir.dx;        // end of edge2
                 y2 = edge2.starty + (EDGE_LEN-1) * edge2.edgeDir.dy;
-                edge2 = new Edge(x2, y2, opposite(edge1.edgeDir), EDGE_LEN, opposite(edge2.edgeDir));
+                edge2 = new Edge(x2, y2, edge2.facing, EDGE_LEN, opposite(edge2.edgeDir));
                 edge1 = new Edge(x1, y1, edge1.edgeDir, EDGE_LEN, edge1.facing);
                 linkEdges(edge1, edge2);
             }
@@ -305,42 +310,24 @@ public class Day22 {
         }
     }
 
-    private void fillEdges() {
 
-//        // block 4R & 6U
-//        pivotEdges(11,8, Dir.UP, Dir.RIGHT, Dir.LEFT, Dir.DOWN);
-//        // block 3D&5L
-//        pivotEdges(8,7, Dir.LEFT, Dir.DOWN, Dir.UP, Dir.LEFT);
-//        // block 3U & 1L
-//        pivotEdges(8,4, Dir.LEFT, Dir.UP, Dir.DOWN, Dir.RIGHT);
+    private boolean teleport(char [][] grid, Player player) {
 
-
-
-//        // block 6D & 2L
-//        Edge edge1 = new Edge(12, 11, Dir.RIGHT, EDGE_LEN, Dir.UP);
-//        Edge edge2 = new Edge(0,7,Dir.UP, EDGE_LEN, Dir.RIGHT);
-//        linkEdges(edge1, edge2);
-//
-//        // block 2D & 5D
-//        edge1 = new Edge(0, 7, Dir.RIGHT, EDGE_LEN, Dir.UP);
-//        edge2 = new Edge(11,11,Dir.LEFT, EDGE_LEN, Dir.UP);
-//        linkEdges(edge1, edge2);
-
-
-    }
-
-    private boolean teleport2(char [][] grid, Player player) {
         for( Edge edge : edges ){
-            int endx = edge.startx + edge.length * edge.edgeDir.dx;
-            int endy = edge.starty + edge.length * edge.edgeDir.dy;
+            int endx = edge.startx + (edge.length-1) * edge.edgeDir.dx;
+            int endy = edge.starty + (edge.length-1) * edge.edgeDir.dy;
             int minx = Math.min(edge.startx, endx);
             int maxx = Math.max(edge.startx, endx);
             int miny = Math.min(edge.starty, endy);
             int maxy = Math.max(edge.starty, endy);
-            if(player.x >= minx && player.x <= maxx && player.y >= miny && player.y <= maxy) {
-                int offset = Math.max(Math.abs(player.x - edge.startx), Math.abs(player.y - edge.starty));
-                Edge newEdge = edge.adjoins;
-                int newx = newEdge.startx + offset*newEdge.edgeDir.dx;
+            // be careful: corners may be on two edges
+            // so look at the player direction
+            if(player.x >= minx && player.x <= maxx && player.y >= miny && player.y <= maxy && player.facing == opposite(edge.facing)) {  // are we on this edge?
+                int offset = Math.max(Math.abs(player.x - edge.startx), Math.abs(player.y - edge.starty));  // relative position on edge
+                if(offset < 0 || offset >= EDGE_LEN)
+                    System.out.println("ERROR: Offset out of bounds");
+                Edge newEdge = edge.adjoins;    // teleport edge
+                int newx = newEdge.startx + offset*newEdge.edgeDir.dx;  // corresponding position on matching edge
                 int newy = newEdge.starty + offset*newEdge.edgeDir.dy;
                 if(grid[newy][newx] == '#')
                     return false;
